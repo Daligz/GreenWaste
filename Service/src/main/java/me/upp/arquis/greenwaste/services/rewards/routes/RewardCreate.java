1a9a6193ef0a31bee44126652a08ce2d991edf99
Dali <oscardali370@gmail.com>
2022-07-07 13:25:08 -0500

    Create for rewards added

--- Service/src/main/java/me/upp/arquis/greenwaste/services/rewards/routes/RewardCreate.java
+++ Service/src/main/java/me/upp/arquis/greenwaste/services/rewards/routes/RewardCreate.java
@@ -1,19 +1,29 @@
 package me.upp.arquis.greenwaste.services.rewards.routes;
 
+import me.upp.arquis.greenwaste.database.Connector;
 import me.upp.arquis.greenwaste.services.RouteWrapper;
+import net.royalmind.library.lightquery.queries.LInsert;
 import spark.Request;
 import spark.Response;
 
 public class RewardCreate extends RouteWrapper {
 
     public RewardCreate() {
-        super("/rewards/create/:id/:name");
+        super("/rewards/create/:name/:desc/:image/:value/:stock");
     }
 
     @Override
     public Object handle(final Request request, final Response response) {
-        final String id = request.params(":id");
         final String name = request.params(":name");
-        return String.format("Id: %s - name: %s", id, name);
+        final String description = request.params(":desc");
+        final String image = request.params(":image");
+        final int value = Integer.parseInt(request.params(":value"));
+        final int stock = Integer.parseInt(request.params(":stock"));
+        final String insert = new LInsert()
+                .table("premios")
+                .values(
+                        null, name, description, image, value, stock
+                ).getQuery();
+        return Connector.HIKARI_POOL.execute(connection -> connection.prepareStatement(insert).execute());
     }
 }
