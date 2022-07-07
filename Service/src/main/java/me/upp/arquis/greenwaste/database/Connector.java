5be8f12f76cd978783f1b5a16e071ff348cfa74c
Dali <oscardali370@gmail.com>
2022-07-07 10:24:13 -0500

    Connector pre-implemented

---
+++ Service/src/main/java/me/upp/arquis/greenwaste/database/Connector.java
@@ -0,0 +1,14 @@
+package me.upp.arquis.greenwaste.database;
+
+import me.upp.daligz.p3.backend.modelo.SimpleSourceBuilder;
+import net.royalmind.library.lightquery.HikariPool;
+
+public class Connector {
+    public static final HikariPool HIKARI_POOL = new HikariPool(
+            new SimpleSourceBuilder()
+                    .setUlr("jdbc:mysql://localhost:3306/database?useSSL=false")
+                    .setUser("root")
+                    .setPassword("")
+                    .build()
+    );
+}
