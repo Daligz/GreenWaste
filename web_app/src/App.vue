911dfbd50cc56f2933c58b4b7438b9adf4202405
saul432 <61369753+saul432@users.noreply.github.com>
2022-07-08 11:10:48 -0500

    Integration of the footer and styles in App.vue

--- web_app/src/App.vue
+++ web_app/src/App.vue
@@ -22,14 +22,27 @@
     </v-main>
     <!---AQUI SE AGREGA EL FOOTER-->
    
-
+<v-footer color="#fff" id="footer">
+      <v-card class="flex" flat tile>
+        <v-card-title>
+          <strong class="subheading footer-text">Green Waste C.V <v-icon>mdi-home-city </v-icon></strong>
+          <v-spacer></v-spacer>
+            <strong class="subheading footer-text">Pachuca de Soto Hidalgo <v-icon>mdi-crosshairs-gps</v-icon></strong>
+        </v-card-title>
+      </v-card>
+    </v-footer>
 
     <!---AQUI SE AGREGA EL FOOTER-->
   </v-app>
 </template>
 <!--AQUI VA LOS ESTILOS DEL FOOTER-->
 
-
+<style>
+.footer-text{
+  font-size: 16px;
+  color: #888888;
+}
+</style>
 
 <!--AQUI VA LOS ESTILOS DEL FOOTER-->
 
