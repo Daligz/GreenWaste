3f207985ebc617610a3a39d4e20cb0db2796c458
Arely Paulina Rojas Esteban <rojas.arelypaulina@micorreo.upp.edu.mx>
2022-07-08 15:34:07 -0500

    Add UpdateUser Screen
    Add component bar
    Add SVG
    Add UpdateUser Screen

--- Movil-app/lib/main.dart
+++ Movil-app/lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: UpdateUser(),
+      home: Login(),
       //const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
