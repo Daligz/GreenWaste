38cbc871409cacc8a6cb9ae2ddb3d6f1f6b11b5d
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-08 05:42:05 -0500

    add components to login
    se agrego los componentes del login, las cajas de textos, imágenes y botón

--- Movil-app/lib/screens/login/components/body_login.dart
+++ Movil-app/lib/screens/login/components/body_login.dart
@@ -6,6 +6,7 @@ import 'package:movil_app/screens/Profile/components/background.dart';
 import 'package:movil_app/components/already_have_a_container_check.dart';
 import 'package:movil_app/components/text_field_container.dart';
 import 'package:movil_app/constants.dart';
+import 'package:movil_app/screens/login/login.dart';
 
 class Body_Login extends StatelessWidget {
 
@@ -22,7 +23,49 @@ class Body_Login extends StatelessWidget {
             Image.asset("assets/imgs/img_main.png",
               height: size.height * 0.45,
             ),
-        
+            TextFieldContainer(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Correo Electronico",
+                  icon: Icon(
+                    Icons.email,
+                    color: kPrimaryDarkColor,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            TextFieldContainer(
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Contraseña",
+                  icon: Icon(
+                    Icons.lock,
+                    color: kPrimaryDarkColor,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            RoundedButton(
+              text: "Inciar Sesión",
+              press: () {},
+            ),
+            RichText(
+              text: TextSpan(
+                text: '¿Aún no tiene cuenta? ',
+                style: DefaultTextStyle.of(context).style,
+                children: const <TextSpan>[
+                  TextSpan(
+                      text: 'Crear Cuenta',
+                      style:
+                      TextStyle(
+                          fontWeight: FontWeight.bold ,
+                          color: kPrimaryColor)),
+                ],
+              ),
+            )
           ],
         ),
       ),
