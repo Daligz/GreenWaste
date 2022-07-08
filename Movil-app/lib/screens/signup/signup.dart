b4f9d9ec453b235a27cd84cddd2ea67f3540e2a5
Giselle Medina <61199330+gmedinax@users.noreply.github.com>
2022-07-08 14:44:22 -0500

    SignUp Interface

---
+++ Movil-app/lib/screens/signup/signup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'components/bodysignup.dart';
+
+class SignUp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BodySignUp(),);
+  }
+}
