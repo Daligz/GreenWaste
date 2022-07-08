2d83650b249a5b09c6d791efffcada8882cda6cb
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-08 10:35:08 -0500

    Creation of the web aplication1
    Se creo el proyecto para la aplicación web con vue.js

---
+++ web_app/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="content-agregarMaterial">
+  <hello-world/>
+  <agregar-materiales/>
+</div>
+
+</template>
+
+<style>
+  .content-agregarMaterial{
+    margin-top:2%;
+    background: #F8F8F8;
+  }
+</style>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+  import AgregarMateriales from '../components/AgregarMateriales'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+      AgregarMateriales,
+    },
+   
+    
+  }
+</script>
