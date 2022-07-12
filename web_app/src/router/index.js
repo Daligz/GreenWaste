d7f06b110f351abf427a3c9f03a04da751c5e928
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-12 00:42:17 -0500

    Adding the Splash route in the web application

--- web_app/src/router/index.js
+++ web_app/src/router/index.js
@@ -19,6 +19,13 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
     }
+  },
+  {
+    path: '/splash',
+    name: 'splash',
+    component: function () {
+      return import('../views/SplashView.vue')
+    }
   }
 ]
 
