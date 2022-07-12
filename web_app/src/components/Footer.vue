18dee67b167407b49b5c0c4aedb60d751e0de851
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-12 00:40:23 -0500

    The footer was passed to the components folder

---
+++ web_app/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+<v-footer color="#fff" id="footer">
+      <v-card class="flex" flat tile>
+        <v-card-title>
+          <strong class="subheading footer-text">Green Waste C.V <v-icon>mdi-home-city </v-icon></strong>
+          <v-spacer></v-spacer>
+            <strong class="subheading footer-text">Pachuca de Soto Hidalgo <v-icon>mdi-crosshairs-gps</v-icon></strong>
+        </v-card-title>
+      </v-card>
+    </v-footer>
+</template>
