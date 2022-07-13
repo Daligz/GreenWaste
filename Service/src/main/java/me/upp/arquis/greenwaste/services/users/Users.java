aa9601296eb2f21c27ff4de494fe39b80acb0d6d
Dali <oscardali370@gmail.com>
2022-07-13 14:16:55 -0500

    Read implemented

--- Service/src/main/java/me/upp/arquis/greenwaste/services/users/Users.java
+++ Service/src/main/java/me/upp/arquis/greenwaste/services/users/Users.java
@@ -3,13 +3,15 @@ package me.upp.arquis.greenwaste.services.users;
 import me.upp.arquis.greenwaste.services.Router;
 import me.upp.arquis.greenwaste.services.users.routes.UserCreate;
 import me.upp.arquis.greenwaste.services.users.routes.UserDelete;
+import me.upp.arquis.greenwaste.services.users.routes.UserRead;
 
 public class Users extends Router {
 
     public Users() {
         super(
                 new UserCreate(),
-                new UserDelete()
+                new UserDelete(),
+                new UserRead()
         );
     }
 }
