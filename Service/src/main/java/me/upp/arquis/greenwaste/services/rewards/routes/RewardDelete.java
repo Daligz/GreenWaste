418394cfd0d0919444c92070c8dc22d52e2ec03d
Dali <oscardali370@gmail.com>
2022-07-13 11:43:52 -0500

    Delete implemented

--- Service/src/main/java/me/upp/arquis/greenwaste/services/rewards/routes/RewardDelete.java
+++ Service/src/main/java/me/upp/arquis/greenwaste/services/rewards/routes/RewardDelete.java
@@ -1,6 +1,8 @@
 package me.upp.arquis.greenwaste.services.rewards.routes;
 
+import me.upp.arquis.greenwaste.database.Connector;
 import me.upp.arquis.greenwaste.services.RouteWrapper;
+import net.royalmind.library.lightquery.queries.LDelete;
 import spark.Request;
 import spark.Response;
 
@@ -13,6 +15,10 @@ public class RewardDelete extends RouteWrapper {
     @Override
     public Object handle(final Request request, final Response response) {
         final String id = request.params(":id");
-        return String.format("Id: %s eliminado!", id);
+        final String delete = new LDelete()
+                .from("premios")
+                .where("idPremio", "=", id)
+                .getQuery();
+        return Connector.HIKARI_POOL.execute(connection -> connection.prepareStatement(delete).execute());
     }
 }
