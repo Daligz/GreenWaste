237e427b4274a8346dcd520f267befe31f69231c
Dali <oscardali370@gmail.com>
2022-07-13 12:13:22 -0500

    Sub services implemented

--- Service/src/main/java/me/upp/arquis/greenwaste/services/rewards/Rewards.java
+++ Service/src/main/java/me/upp/arquis/greenwaste/services/rewards/Rewards.java
@@ -3,12 +3,16 @@ package me.upp.arquis.greenwaste.services.rewards;
 import me.upp.arquis.greenwaste.services.Router;
 import me.upp.arquis.greenwaste.services.rewards.routes.RewardCreate;
 import me.upp.arquis.greenwaste.services.rewards.routes.RewardDelete;
+import me.upp.arquis.greenwaste.services.rewards.routes.RewardRead;
+import me.upp.arquis.greenwaste.services.rewards.routes.RewardUpdate;
 
 public class Rewards extends Router {
 
     public Rewards() {
         super(
                 new RewardCreate(),
+                new RewardRead(),
+                new RewardUpdate(),
                 new RewardDelete()
         );
     }
