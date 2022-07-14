aed41c5f5a1218dfcfc9f5f5805fe9ec794872fa
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-14 00:17:02 -0500

    create login service route

--- Movil-app/lib/service/service_routes.dart
+++ Movil-app/lib/service/service_routes.dart
@@ -6,4 +6,7 @@ class Routes{
       final String apellidoPaterno, final String apellidoMaterno,
       final String telefono, final String correo, final String psw,
       final int idRol) => _url + "/users/create/$nombre/$apellidoPaterno/$apellidoMaterno/$telefono/$correo/$psw/$idRol";
+
+  static String routeUserLogin(final String correo, final String password) => _url + "/users/login/$correo/$password";
+
 }
