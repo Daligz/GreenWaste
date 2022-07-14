a9247a840fd51a2884399a97583b354219503671
Giselle Medina <61199330+gmedinax@users.noreply.github.com>
2022-07-13 22:53:09 -0500

    Fixing route

--- Movil-app/lib/service/service.dart
+++ Movil-app/lib/service/service.dart
@@ -5,7 +5,8 @@ class Service {
   Future<bool> createUser(final String nombre,
       final String apellidoPaterno, final String apellidoMaterno,
       final String telefono, final String correo, final String psw) async{
-    final Response response = await get(Uri.parse(Routes.routeUserCreate(nombre, apellidoPaterno, apellidoMaterno, telefono, correo, psw, 1)));
+    //final Response response = await get(Uri.parse(Routes.routeUserCreate(nombre, apellidoPaterno, apellidoMaterno, telefono, correo, psw, 1)));
+    final Response response = await get(Uri.parse("http://localhost:6969/users/create/$nombre/$apellidoPaterno/$apellidoMaterno/$telefono/$correo/$psw/1"));
     return (response.statusCode == 200);
   }
 }
