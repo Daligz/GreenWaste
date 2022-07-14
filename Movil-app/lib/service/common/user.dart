4e78f7fa0999c28918a53eea947cce2d4f318082
Giselle Medina <61199330+gmedinax@users.noreply.github.com>
2022-07-13 19:45:48 -0500

    Error corrections in user object

--- Movil-app/lib/service/common/user.dart
+++ Movil-app/lib/service/common/user.dart
@@ -1,11 +1,12 @@
-class Post {
+class User {
   final String nombre, apellidoPaterno, apellidoMaterno, telefono, correo, psw;
-  final int idRol;
+  final int idUser, idRol;
 
-  Post(this.nombre, this.apellidoPaterno, this.apellidoMaterno, this.telefono, this.correo, this.psw, this.idRol);
+  User(this.idUser, this.nombre, this.apellidoPaterno, this.apellidoMaterno, this.telefono, this.correo, this.psw, this.idRol);
 
-  factory Post.fromJson(final Map<String, dynamic> jsonMap) {
-    return Post(
+  factory User.fromJson(final Map<String, dynamic> jsonMap) {
+    return User(
+      jsonMap['idUser'],
       jsonMap['nombre'],
       jsonMap['apellidoPaterno'],
       jsonMap['apellidoMaterno'],
