9eb3ba70252442ddc74dceeb358c53cb158a6bca
saul432 <61369753+saul432@users.noreply.github.com>
2022-07-19 18:05:39 -0500

    Create MaterialUpdate.java
    Method of update material implemented

---
+++ Service/src/main/java/me/upp/arquis/greenwaste/services/materials/routes/MaterialUpdate.java
@@ -0,0 +1,29 @@
+package me.upp.arquis.greenwaste.services.materials.routes;
+
+import me.upp.arquis.greenwaste.database.Connector;
+import me.upp.arquis.greenwaste.services.RouteWrapper;
+import net.royalmind.library.lightquery.queries.LUpdate;
+import spark.Request;
+import spark.Response;
+
+public class MaterialUpdate extends RouteWrapper {
+
+    public MaterialUpdate() {
+        super("/materials/update/:id/:name/:cost");
+    }
+
+    @Override
+    public Object handle(final Request request, final Response response){
+        final String id = request.params(":id");
+        final String name = request.params(":name");
+        final String cost = request.params(":cost");
+
+        final String update = new LUpdate()
+                .table("material")
+                .update("nombre", name)
+                .update("costo", cost)
+                .where("idMaterial", "=", id)
+                .getQuery();
+        return Connector.HIKARI_POOL.execute(connection -> !(connection.prepareStatement(update).execute()));
+    }
+}
