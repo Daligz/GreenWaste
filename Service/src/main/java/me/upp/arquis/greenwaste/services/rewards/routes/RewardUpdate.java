a98b64471b9dab687eaf80f392db9a13c1c2d2e5
Dali <oscardali370@gmail.com>
2022-07-21 17:59:25 -0500

    Added all parameters to update class

--- Service/src/main/java/me/upp/arquis/greenwaste/services/rewards/routes/RewardUpdate.java
+++ Service/src/main/java/me/upp/arquis/greenwaste/services/rewards/routes/RewardUpdate.java
@@ -10,17 +10,24 @@ import spark.Response;
 public class RewardUpdate extends RouteWrapper {
 
     public RewardUpdate() {
-        super("/rewards/update/:id/:value/:valueToUpdate");
+        super("/rewards/update/:id/:name/:desc/:image/:value/:stock");
     }
 
     @Override
     public Object handle(final Request request, final Response response) {
         final String id = request.params(":id");
-        final String value = request.params(":value");
-        final String valueToUpdate = request.params(":valueToUpdate");
+        final String name = request.params(":name");
+        final String description = request.params(":desc");
+        final String image = request.params(":image");
+        final int value = Integer.parseInt(request.params(":value"));
+        final int stock = Integer.parseInt(request.params(":stock"));
         final String update = new LUpdate()
                 .table("premios")
-                .update(value, valueToUpdate)
+                .update("nombre", name)
+                .update("descripcion", description)
+                .update("imagen", image)
+                .update("valor", value)
+                .update("stock", stock)
                 .where("idPremio", "=", id)
                 .getQuery();
         return Connector.HIKARI_POOL.execute(connection -> !(connection.prepareStatement(update).execute()));
