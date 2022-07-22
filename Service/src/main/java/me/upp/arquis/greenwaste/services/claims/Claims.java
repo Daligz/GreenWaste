7c519b48d9c743bb38212216825e40423e74bb69
Dali <oscardali370@gmail.com>
2022-07-22 15:35:40 -0500

    Claims added

---
+++ Service/src/main/java/me/upp/arquis/greenwaste/services/claims/Claims.java
@@ -0,0 +1,12 @@
+package me.upp.arquis.greenwaste.services.claims;
+
+import me.upp.arquis.greenwaste.services.Router;
+
+public class Claims extends Router {
+
+    public Claims() {
+        super(
+                
+        );
+    }
+}
