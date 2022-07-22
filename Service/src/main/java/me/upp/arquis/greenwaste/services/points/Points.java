c7c4c93acc32f2b3afc563c5d6a0e1ecf0abf1ae
Dali <oscardali370@gmail.com>
2022-07-22 14:44:52 -0500

    Points added to service

---
+++ Service/src/main/java/me/upp/arquis/greenwaste/services/points/Points.java
@@ -0,0 +1,12 @@
+package me.upp.arquis.greenwaste.services.points;
+
+import me.upp.arquis.greenwaste.services.Router;
+
+public class Points extends Router {
+
+    public Points() {
+        super(
+                
+        );
+    }
+}
