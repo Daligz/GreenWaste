9e18eaba886f02f35aa6e3f02018e02d884318cc
Dali <oscardali370@gmail.com>
2022-08-02 19:24:49 -0500

    Share button added and text list implemented

--- Movil-app/lib/screens/reports/components/reports_body.dart
+++ Movil-app/lib/screens/reports/components/reports_body.dart
@@ -34,59 +34,76 @@ class ReportBody extends StatelessWidget {
       color: Color(0x7D000000)
     );
     const TextStyle pointsText = TextStyle(
-      fontSize: 20.0,
+      fontSize: 15.0,
       color: Color(0xFF000000)
     );
     initializeDateFormatting();
     Intl.defaultLocale = 'es';
     String monthName = DateFormat('MMMM').format(DateTime(0, DateTime.now().month)).toCapitalized();
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget> [
-          const SizedBox(height: 50.0),
-          const Text(
-            'Historial de Recompensas',
-            style: titleStyle
-          ),
-          Text(
-            monthName,
-            style: subtitleStyle
-          ),
-          const SizedBox(height: 50.0),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 75.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            const SizedBox(height: 50.0),
+            const Text(
+                'Historial de Recompensas',
+                style: titleStyle
+            ),
+            Text(
+                monthName,
+                style: subtitleStyle
+            ),
+            const SizedBox(height: 50.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 95.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget> [
+                  Text(
+                      '306 puntos',
+                      style: pointsStyle
+                  ),
+                  Text(
+                      'Utilizados',
+                      style: pointsText
+                  ),
+                  Text(
+                      'durante este mes',
+                      style: pointsText
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 50.0),
+            const Text(
+                'Lista de recompensas obtenidas durante este mes',
+                style: pointsText
+            ),
+            const SizedBox(height: 25.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: const <Widget> [
                 Text(
-                  '306 puntos',
-                  style: pointsStyle
+                    'Linterna'
                 ),
+                SizedBox(width: 50.0),
                 Text(
-                  'Utilizados',
-                    style: pointsText
-                ),
-                Text(
-                  'durante este mes',
-                    style: pointsText
+                    '89 pts'
                 )
               ],
             ),
-          ),
-          const SizedBox(height: 50.0),
-          const Text(
-              'Lista de recompensas obtenidas durante este mes'
-          ),
-          const Text(
-              'Linterna'
-          ),
-          const Text(
-              '89 pts'
-          ),
-        ],
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        tooltip: 'Compartir',
+        backgroundColor: const Color(0xFF358F80),
+        foregroundColor: const Color(0xFFF4F0F0),
+        child: const Icon(Icons.share)
       )
     );
   }
