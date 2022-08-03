e4e2594a6cae6ca5ba8bbf3ffa8aa915c623402b
Dali <oscardali370@gmail.com>
2022-08-03 17:08:14 -0500

    Shiny server added

--- DataMining/knn-proyecto.R
+++ DataMining/knn-proyecto.R
@@ -49,3 +49,45 @@ tabla   <- table(prediction, factor(prueba$ventas))
 tabla
 
 boxplot(df$producto ~ factor(df$ventas), data = df, col = c("yellow"))
+
+library(shiny)
+
+ui <- fluidPage(
+  
+  titlePanel("NO SE QUE TITULO  PONER"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+      
+    ),
+    
+    mainPanel(
+      
+      plotOutput(outputId = "distPlot")
+      
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
+    
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
