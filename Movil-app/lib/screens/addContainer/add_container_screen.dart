aa0fd45aa5964648a5f7ec95c33f0a001d344649
Giselle Medina <61199330+gmedinax@users.noreply.github.com>
2022-08-04 01:24:11 -0500

    View edited

--- Movil-app/lib/screens/addContainer/add_container_screen.dart
+++ Movil-app/lib/screens/addContainer/add_container_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../../service/common/user.dart';
 import 'components/body.dart';
 
-class AddContainer extends StatelessWidget{
+class AddContainerScreen extends StatelessWidget{
+  final User? user;
+
+  const AddContainerScreen(
+      {Key? key,
+        required this.user})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(),
+      body: Body(
+        user: user,
+      ),
     );
   }
 }
--- Movil-app/lib/screens/addContainer/components/body.dart
+++ Movil-app/lib/screens/addContainer/components/body.dart
@@ -2,42 +2,78 @@ import 'package:flutter/material.dart';
 import 'package:movil_app/components/rounded_input_field.dart';
 import 'package:movil_app/screens/addContainer/components/background.dart';
 
-class Body extends StatelessWidget {
-  String? selectedValue = null;
-  final _dropdownFormKey = GlobalKey<FormState>();
+import '../../../service/common/user.dart';
+
+class Body extends StatefulWidget {
+  final User? user;
+
+  const Body(
+      {Key? key,
+        required this.user})
+      : super(key: key);
+
+  @override
+  _BodyState createState() => new _BodyState();
+}
+
+class _BodyState extends State<Body> {
+
+  List _cities =
+  ["Cluj-Napoca", "Bucuresti", "Timisoara", "Brasov", "Constanta"];
+
+  late List<DropdownMenuItem<String>> _dropDownMenuItems;
+  late String _currentCity;
+
+  @override
+  void initState() {
+    _dropDownMenuItems = getDropDownMenuItems();
+    _currentCity = _dropDownMenuItems[0].value!;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<String>> getDropDownMenuItems() {
+    List<DropdownMenuItem<String>>items= [];
+    for (String city in _cities) {
+      items.add(new DropdownMenuItem(
+          value: city,
+          child: new Text(city)
+      ));
+    }
+    return items;
+  }
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController materialTextController = TextEditingController();
     Size size = MediaQuery.of(context).size;
-    return Background(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Container(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(
+              height: 100,
+            ),
             Text(
-              "Agregar contenedor",
-              style: Theme.of(context).textTheme.headline2,
-            ),
-            Image.asset(
-              "assets/imgs/img_agregarContenedor_movil.png",
-              height: size.height * 0.45,
-            ),
-            RoundedInputField(
-                hintText: "Material",
-                icon: Icons.delete,
-                onChanged: (value) {},
-                controller: materialTextController
-            ),
-            /*DropdownButtonFormField(
+            "Agregar contenedor",
+            style: Theme.of(context).textTheme.headline2,
+          ),
+          Image.asset(
+            "assets/imgs/img_agregarContenedor_movil.png",
+            height: size.height * 0.45,
+          ),
 
-                  ),*/
-            /*DropdownButtonFormField(
-
-                  ),*/
           ],
-        ),
       ),
+
+    ),
     );
   }
+
+  void changedDropDownItem(String selectedCity) {
+    setState(() {
+      _currentCity = selectedCity;
+    });
+  }
+
 }
