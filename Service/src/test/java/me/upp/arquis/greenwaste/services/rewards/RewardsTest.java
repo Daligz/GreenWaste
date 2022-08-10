ef514e8869363a2b50aa028116e1b20f19a5fa3e
Daligz <oscardali370@gmail.com>
2022-08-10 11:23:26 -0500

    Rewards test structure created

---
+++ Service/src/test/java/me/upp/arquis/greenwaste/services/rewards/RewardsTest.java
@@ -0,0 +1,26 @@
+package me.upp.arquis.greenwaste.services.rewards;
+
+import org.junit.jupiter.api.Test;
+
+class RewardsTest {
+
+    @Test
+    public void create() {
+
+    }
+
+    @Test
+    public void read() {
+
+    }
+
+    @Test
+    public void update() {
+
+    }
+
+    @Test
+    public void delete() {
+
+    }
+}
