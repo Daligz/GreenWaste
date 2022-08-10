c7d37c94dc37c58363227115e44fefb0e5d76a48
Daligz <oscardali370@gmail.com>
2022-08-10 11:47:34 -0500

    Test implemented

--- Service/src/test/java/me/upp/arquis/greenwaste/services/rewards/RewardsTest.java
+++ Service/src/test/java/me/upp/arquis/greenwaste/services/rewards/RewardsTest.java
@@ -1,29 +1,38 @@
 package me.upp.arquis.greenwaste.services.rewards;
 
-import me.upp.arquis.greenwaste.services.rewards.routes.RewardCreate;
-import org.json.HTTP;
+import me.upp.arquis.greenwaste.services.utils.Request;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RewardsTest {
 
+    private static final String URL = "http://localhost:6969";
+
     @Test
-    public void create() {
-        assertEquals("true", );
+    public void create() throws IOException {
+        final HttpURLConnection connection = Request.doRequest(URL + "/rewards/create/test/descTest/imageTest/0/0");
+        assertEquals(200, connection.getResponseCode());
     }
 
     @Test
-    public void read() {
-
+    public void read() throws IOException {
+        final HttpURLConnection connection = Request.doRequest(URL + "/rewards/read");
+        assertEquals(200, connection.getResponseCode());
     }
 
     @Test
-    public void update() {
-
+    public void update() throws IOException {
+        final HttpURLConnection connection = Request.doRequest(URL + "/rewards/update/2/testEdit/descEdit/imageEdit/1/1");
+        assertEquals(200, connection.getResponseCode());
     }
 
     @Test
-    public void delete() {
-
+    public void delete() throws IOException {
+        final HttpURLConnection connection = Request.doRequest(URL + "/rewards/delete/3");
+        assertEquals(200, connection.getResponseCode());
     }
 }
