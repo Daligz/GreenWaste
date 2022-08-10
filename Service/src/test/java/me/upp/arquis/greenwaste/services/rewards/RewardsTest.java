b3ed7a4c4ca80124b7b0b9852102cd454a0384c5
Daligz <oscardali370@gmail.com>
2022-08-10 11:34:20 -0500

    Request util added to test

--- Service/src/test/java/me/upp/arquis/greenwaste/services/rewards/RewardsTest.java
+++ Service/src/test/java/me/upp/arquis/greenwaste/services/rewards/RewardsTest.java
@@ -1,12 +1,15 @@
 package me.upp.arquis.greenwaste.services.rewards;
 
+import me.upp.arquis.greenwaste.services.rewards.routes.RewardCreate;
+import org.json.HTTP;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RewardsTest {
 
     @Test
     public void create() {
-
+        assertEquals("true", );
     }
 
     @Test
